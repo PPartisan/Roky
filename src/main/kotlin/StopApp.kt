78306f5a49c76dd930ccfbf438bf6c12b601c05d
Tom Calver <tom.calver@hey.com>
2024-10-19 11:35:23 +0100

    7: Address detekt errors

--- src/main/kotlin/StopApp.kt
+++ src/main/kotlin/StopApp.kt
@@ -2,13 +2,11 @@ import com.googlecode.lanterna.gui2.MultiWindowTextGUI
 import com.googlecode.lanterna.screen.Screen
 
 class StopApp (
-   private val screen: Screen,
+    private val screen: Screen,
     private val multiWindowTextGUI: MultiWindowTextGUI
 ) {
-
-        operator fun invoke (){
-            multiWindowTextGUI.removeWindow(multiWindowTextGUI.activeWindow)
-            screen.stopScreen()
-
-        }
+    operator fun invoke (){
+        multiWindowTextGUI.removeWindow(multiWindowTextGUI.activeWindow)
+        screen.stopScreen()
+    }
 }
