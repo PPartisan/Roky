6bf754ba61c88e696b35dba5bd8fccfcae7afb44
Brian Gurdas <briangurdas@brians-mbp.home>
2024-11-19 20:44:24 +0000

    #46:Started help package structure

---
+++ src/main/kotlin/help/HelpPresenter.kt
@@ -0,0 +1,15 @@
+package help
+
+import arch.Presenter
+import arch.RokyDispatchers
+
+class HelpPresenter(dispatchers: RokyDispatchers) : Presenter<HelpView>(dispatchers) {
+    override fun onAttach(view: HelpView) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDetach(view: HelpView) {
+        TODO("Not yet implemented")
+    }
+
+}
