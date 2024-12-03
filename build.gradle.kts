8b71660063133e477ea02e8a90deb8f06a1c41aa
Robert-Davie <robertjdavie2@gmail.com>
2024-12-03 20:23:09 +0000

    #60: fetch markdown from github gist

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,10 @@ repositories {
 }
 
 dependencies {
+    val ktorVersion="3.0.0"
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+
     implementation("com.googlecode.lanterna:lanterna:3.1.1")
     implementation("com.vladsch.flexmark:flexmark-all:0.64.8")
 
