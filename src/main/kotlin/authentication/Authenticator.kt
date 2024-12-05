11142549152cbfe4c3d97ddd4fd1ea026a975122
Niamh <niamhlongley@gmail.com>
2024-12-05 20:06:59 +0000

    #64: Updated the list of mock users.

--- src/main/kotlin/authentication/Authenticator.kt
+++ src/main/kotlin/authentication/Authenticator.kt
@@ -18,7 +18,7 @@ class Authenticator {
     }
 
     companion object {
-        private val validUsers = listOf("Robert", "Dunia", "Tom", "Max", "Casper", "Ed", "Kai", "Laura")
+        private val validUsers = listOf("Robert", "Dunia", "Tom", "Max", "Casper", "Ed", "Kai", "Laura", "Niamh")
         private const val PASSWORD = "ILoveRoky"
     }
 
