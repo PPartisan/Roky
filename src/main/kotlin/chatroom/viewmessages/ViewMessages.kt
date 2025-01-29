0b7232ede3aed33b96383ae7d26466c2adab181b
sorellla <martinemarrelli@gmail.com>
2025-01-29 18:54:14 +0100

    #102: start view Messages Presenter

--- src/main/kotlin/chatroom/viewmessages/ViewMessages.kt
+++ src/main/kotlin/chatroom/viewmessages/ViewMessages.kt
@@ -6,6 +6,12 @@ import org.koin.dsl.module
 val viewMessagesModule =
     module {
         scope<ChatroomWindow> {
-            scoped { ViewMessagesPanel() }
+            scoped { ViewMessagesPanel(get()) }
+            scoped {
+                ViewMessagesPresenter(
+                    windowScope = get<ChatroomWindow>().windowScope,
+                    dispatchers = get(),
+                )
+            }
         }
     }
--- src/main/kotlin/chatroom/viewmessages/ViewMessagesPresenter.kt
+++ src/main/kotlin/chatroom/viewmessages/ViewMessagesPresenter.kt
@@ -1,3 +1,56 @@
 package chatroom.viewmessages
 
-class ViewMessagesPresenter
+import arch.Presenter
+import arch.RokyDispatchers
+import chatroom.viewmessages.ViewMessagesViewState.NoMessages
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.time.Duration.Companion.seconds
+
+class ViewMessagesPresenter(
+    private val windowScope: CoroutineScope,
+    dispatchers: RokyDispatchers,
+) : Presenter<ViewMessagesView>(dispatchers) {
+    override fun onAttach(view: ViewMessagesView) {
+        view.show(NoMessages)
+        windowScope.launch(dispatchers.default) {
+            while (true) {
+                delay(3.seconds)
+                windowScope.launch(dispatchers.main) {
+                    withView {
+                        it.show(
+                            ViewMessagesViewState.Messages(
+                                "${sampleUsers.random()}: ${sampleMessages.random()}",
+                            ),
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onDetach(view: ViewMessagesView) {
+        TODO("Not yet implemented")
+    }
+
+    companion object {
+        private val sampleMessages =
+            listOf(
+                "This is a coup!",
+                "What time's Roky Coding tonight?",
+                "Look at the calendar...",
+                "Charizard",
+                "Remind me to get my washing at 4 PM",
+            )
+        private val sampleUsers =
+            listOf(
+                "Martine",
+                "Ed",
+                "Kai",
+                "Terry",
+                "Robert",
+                "Tom",
+            )
+    }
+}
