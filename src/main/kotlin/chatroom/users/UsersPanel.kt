32cbcffefe43c6bc07f042585c2dc68ecedad9ac
Mike <michael_jim@hotmail.co.uk>
2025-02-21 17:58:27 +0000

    #125: add users list presenter

--- src/main/kotlin/chatroom/users/UsersPanel.kt
+++ src/main/kotlin/chatroom/users/UsersPanel.kt
@@ -1,7 +1,7 @@
 package chatroom.users
 
-import arch.RokyDispatchers
 import chatroom.BorderedPanel
+import chatroom.users.UsersViewState.*
 import com.googlecode.lanterna.gui2.Border
 import com.googlecode.lanterna.gui2.BorderLayout
 import com.googlecode.lanterna.gui2.BorderLayout.Location.CENTER
@@ -9,14 +9,10 @@ import com.googlecode.lanterna.gui2.Borders
 import com.googlecode.lanterna.gui2.Label
 import com.googlecode.lanterna.gui2.Panel
 import com.googlecode.lanterna.gui2.TextBox
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 
 class UsersPanel(
-    useCase: UsersListUseCase,
-    scope: CoroutineScope,
-    dispatcher: RokyDispatchers,
-) : Panel(BorderLayout()), BorderedPanel {
+    presenter: UsersListPresenter,
+) : Panel(BorderLayout()), BorderedPanel, UsersListView {
     private val empty = Label("Loading...").setLayoutData(CENTER)
     private val users = TextBox().setLayoutData(CENTER).setReadOnly(true)
 
@@ -24,16 +20,29 @@ class UsersPanel(
         addComponent(empty)
         addComponent(users)
         users.isVisible = false
-        scope.launch(dispatcher.main) {
-            useCase().collect {
-                users.isVisible = true
-                empty.isVisible = false
-                users.text = it.joinToString("\n")
-            }
-        }
+        presenter.attach(this)
     }
 
     override fun bordered(): Border {
         return withBorder(Borders.singleLine("Users"))
+    }
+
+    override fun show(state: UsersViewState) {
+        when (state) {
+            is Empty -> onEmpty()
+            is Users -> onUsers(state)
+        }
+    }
+
+    private fun onUsers(state: Users) {
+        empty.isVisible = false
+        users.isVisible = true
+        users.text = state.users.joinToString("\n")
+    }
+
+    private fun onEmpty() {
+        empty.isVisible = true
+        users.isVisible = false
+        users.text = ""
     }
 }
