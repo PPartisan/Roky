87ad6c4ad4bb7a470ab8df3b0c42833377a47b55
Mike <michael_jim@hotmail.co.uk>
2025-04-03 19:49:27 +0100

    #143: support remote login on supabase

--- src/main/kotlin/authentication/RemoteAuth.kt
+++ src/main/kotlin/authentication/RemoteAuth.kt
@@ -1,32 +1,75 @@
 package authentication
 
-import kotlinx.coroutines.delay
-import kotlin.time.Duration.Companion.seconds
+import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.auth.auth
+import io.github.jan.supabase.auth.providers.builtin.Email
+import io.github.jan.supabase.auth.status.SessionStatus
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-// SUPABASEEEE
-object RemoteAuth : Auth {
-    private var isLoggedIn: Boolean = false
+class RemoteAuth(
+    private val client: SupabaseClient,
+    private val scope: CoroutineScope,
+) : Auth {
+    private val _state: MutableStateFlow<AuthState> = MutableStateFlow(AuthState.InitState)
+    val state: StateFlow<AuthState> = _state.asStateFlow()
+
+    init {
+        scope.launch {
+            client.auth.sessionStatus.collect {
+                _state.value = it.toRokyState()
+                println("Current State: ${state.value}")
+            }
+        }
+    }
 
     override suspend fun login(
         email: String,
         password: String,
     ) {
-        println("Remote Auth login")
-        delay(3.seconds)
-        isLoggedIn = (email in validUsers && password == PASSWORD)
+        _state.value = AuthState.Authenticating
+        try {
+            client.auth.signInWith(Email) {
+                this.email = email
+                this.password = password
+            }
+        } catch (e: Exception) {
+            _state.value = AuthState.InvalidCredentials
+        }
     }
 
     override suspend fun logout() {
         println("Remote Auth logout")
-        isLoggedIn = false
     }
 
     override suspend fun isLoggedIn(): Boolean {
         println("Remote Auth is logged in")
-        return isLoggedIn
+        return state.value is AuthState.SignIn
     }
 
-    private val validUsers =
-        listOf("Robert", "Dunia", "Tom", "Max", "Casper", "Ed", "Kai", "Laura", "Niamh", "Sofia")
-    private const val PASSWORD = "ILoveRoky"
+    companion object {
+        fun SessionStatus.toRokyState(): AuthState =
+            when (this) {
+                is SessionStatus.Authenticated -> AuthState.SignIn(session.user?.id ?: "Unknown User")
+                is SessionStatus.NotAuthenticated -> AuthState.InvalidCredentials
+                else -> {
+                    println("Unrecognised State: $this")
+                    AuthState.InvalidCredentials
+                }
+            }
+    }
+}
+
+sealed interface AuthState {
+    data class SignIn(val user: String) : AuthState
+
+    data object InvalidCredentials : AuthState
+
+    data object Authenticating : AuthState
+
+    data object InitState : AuthState
 }
