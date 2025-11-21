c6430c31312dab9d0393c2050d9fa079211ddf4d
sorellla <martinemarrelli@gmail.com>
2025-11-21 10:45:36 +0100

    #130: LocalAuth back to normal

--- src/main/kotlin/authentication/LocalAuth.kt
+++ src/main/kotlin/authentication/LocalAuth.kt
@@ -35,7 +35,7 @@ object LocalAuth : Auth, ReadAuth {
 
     private val validUsers =
         listOf("Robert", "Dunia", "Tom", "Max", "Casper", "Ed", "Kai", "Laura", "Niamh", "Sofia")
-    private const val PASSWORD = "1"
+    private const val PASSWORD = "ILoveRoky"
 
     override fun state(): Flow<AuthState> = state
 
