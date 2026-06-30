ed7ac896ada6a3c0d052da532385644e74d37a7c
KaiRaiChu <kaic3116@gmail.com>
2026-06-30 17:56:04 +0100

    #181: add tests for displayble messages

--- src/main/kotlin/chatroom/viewmessages/DisplayableMessages.kt
+++ src/main/kotlin/chatroom/viewmessages/DisplayableMessages.kt
@@ -4,19 +4,14 @@ import chatserver.ChatRepositories
 import kotlinx.coroutines.flow.*
 
 class DisplayableMessages(private val repositories: ChatRepositories) {
-    operator fun invoke(): Flow<String> {
-        val messages =
-            repositories.readMessages().observe()
-                .filter { it.isOk }
-                .map { it.item.lastOrNull() }
-                .filterNotNull()
-        val username =
-            repositories.readProfiles().observe()
-                .filter { it.isOk }
-                .map { it.item }
-        return combine(messages, username) { message, usernames ->
-            val user = usernames[message.userId]?.username ?: "anon"
-            "$user: ${message.message}"
-        }
-    }
+    operator fun invoke(): Flow<String> =
+        repositories.readMessages().observe()
+            .filter { it.isOk }
+            .map { it.item.lastOrNull() }
+            .filterNotNull()
+            .map { message ->
+                val usernames = repositories.readProfiles().latest().takeIf { it.isOk }?.item ?: emptyMap()
+                val user = usernames[message.userId]?.username ?: "anon"
+                "$user: ${message.message}"
+            }
 }
