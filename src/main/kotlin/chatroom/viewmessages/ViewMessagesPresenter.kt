4f600e3dce3e46ce2ca3d165b56c78afa4cec431
Thomas Olive-Calver <PPartisan@users.noreply.github.com>
2026-06-30 18:04:37 +0100

    Revert "Chore/bump release 0.1.1 to 0.1.2"

--- src/main/kotlin/chatroom/viewmessages/ViewMessagesPresenter.kt
+++ src/main/kotlin/chatroom/viewmessages/ViewMessagesPresenter.kt
@@ -4,26 +4,33 @@ import arch.Presenter
 import arch.RokyDispatchers
 import chatroom.viewmessages.ViewMessagesViewState.Messages
 import chatroom.viewmessages.ViewMessagesViewState.NoMessages
+import chatserver.MessageResult
+import chatserver.ReadChatRepository
 import chatserver.SubscribeChatRepository
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class ViewMessagesPresenter(
     private val windowScope: CoroutineScope,
-    private val read: DisplayableMessages,
+    private val read: ReadChatRepository<MessageResult>,
     private val channel: SubscribeChatRepository,
     dispatchers: RokyDispatchers,
 ) : Presenter<ViewMessagesView>(dispatchers) {
     override fun onAttach(view: ViewMessagesView) {
         view.show(NoMessages)
         windowScope.launch(dispatchers.io) {
-            read().map(::Messages).collect { message ->
-                withContext(dispatchers.main) {
-                    withView { it.show(message) }
+            read.observe()
+                .filter { it.isOk }
+                .map { it.item.lastOrNull()?.let { "${it.userId}: ${it.message}" }.orEmpty() }
+                .map(::Messages)
+                .collect { message ->
+                    withContext(dispatchers.main) {
+                        withView { it.show(message) }
+                    }
                 }
-            }
         }
         channel.subscribe()
     }
